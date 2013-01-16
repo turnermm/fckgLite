7bfda722c64249e3c8799142e0ef7341b96b3a3d
kodstark <kodstark@gmail.com>
2013-01-16 15:44:50 +0000

    - Add strip HTML possiblity to Geshi dialog. Strip HTML is useful when pasting code from other site, even when it uses Geshi as well. It has similar effect to pasting clean text from text editor like Gedit or Notepad++.  Just paste code from browser to fckgLite editor, select it and run Geshi with stripping HTML.
    - Add saving / restoring from cookie last selected lang in Geshi dialog.

--- fckg/fckeditor/editor/plugins/geshi/fckplugin.js
+++ fckg/fckeditor/editor/plugins/geshi/fckplugin.js
@@ -1,6 +1,7 @@
 /*
  * FCKeditor - The text editor for Internet - http://www.fckeditor.net
  * Copyright (C) 2003-2007 Frederico Caldeira Knabben
+ * Copyright (C) 2013 Kamil Demecki [kodstark@gmail.com]
  *
  * == BEGIN LICENSE ==
  *
@@ -35,8 +36,7 @@ FCKToolbarItems.RegisterItem( 'geshi', oGeshiTool ) ;
 // The object used for all Geshi operations.
 var FCKGeshi = new Object() ;
 
-FCKGeshi.Insert = function(code_type, lang, snip_fname, isSafari) {
-    
+FCKGeshi.Insert = function(code_type, lang, snip_fname, isSafari, strip_html) {    
   
     var pre_class = code_type + ' ' + lang;
 	var hrefStartHtml	=  '<pre class="' + pre_class + '">';
@@ -48,14 +48,18 @@ FCKGeshi.Insert = function(code_type, lang, snip_fname, isSafari) {
 
     mySelection = mySelection.replace(/^\s+/,"");
     mySelection = mySelection.replace(/\s+$/,"");
+    if (strip_html == "re") {
+        mySelection = this.StripHtmlRe(mySelection);        
+    } else if (strip_html == "dom") {
+        mySelection = this.StripHtmlDom(mySelection);
+    }
+
     if(!mySelection) mySelection = "<br /><br />";
 
     hrefHtml = hrefStartHtml+mySelection+hrefEndHtml;
     if(snip_fname) {
         hrefHtml=downloadable_snippet(hrefHtml, snip_fname) ;
-    }
-
-   
+    }   
 
     hrefHtml += '<span class="np_break">&nbsp;</span>';
     if(isSafari) {
@@ -65,18 +69,15 @@ FCKGeshi.Insert = function(code_type, lang, snip_fname, isSafari) {
 }
 
 FCKGeshi.InsertEdited = function(val) {
-
-//	mySelection = ( FCKBrowserInfo.IsIE) ? FCKSelection.GetSelectedHTML() : removeBR(FCKSelection.GetSelectedHTML());
+    //	mySelection = ( FCKBrowserInfo.IsIE) ? FCKSelection.GetSelectedHTML() : removeBR(FCKSelection.GetSelectedHTML());
 
 	hrefHtml = val;
-
 
 	FCK.InsertHtml(hrefHtml);
 }
 
 FCKSelection.GetSelectedHTML = function(isSafari) {	
-
-							// see http://www.quirksmode.org/js/selected.html for other browsers
+    // see http://www.quirksmode.org/js/selected.html for other browsers
 	if( FCKBrowserInfo.IsIE) {												// IE
 		var oRange = FCK.EditorDocument.selection.createRange() ;
 		//if an object like a table is deleted, the call to GetType before getting again a range returns Control
@@ -105,6 +106,121 @@ FCKSelection.GetSelectedHTML = function(isSafari) {
 	}
 }
 
+/* Strip html tags with preserving whitespaces and assuming that br, p and div introduce new line */
+FCKGeshi.StripHtmlRe = function(htmlText) {
+    var result = htmlText;    
+    result = result.replace(/<br\/?>/g, "\n");
+    result = result.replace(/<p.*?>/gm, "\n");
+    result = result.replace(/<div.*?>/gm, "\n");
+    result = result.replace(/<(?:.|\n)*?>/gm, '');
+    return result;
+}
+
+// originally from Club AJAX General Purpose Code 
+// author of original: Mike Wilcox http://clubajax.org
+// Returns a line-break, properly spaced, normailized plain text
+// representation of multiple child nodes which can't be done via
+// textContent or innerText because those two methods are vastly
+// different, and even innerText works differently across browsers.
+FCKGeshi.StripHtmlDom = function(htmlText){
+    var node = document.createElement("div");
+    node.innerHTML = htmlText;
+
+	// used for testing:
+	// return node.innerText || node.textContent;
+
+	var normalize = function(a){
+		// clean up double line breaks and spaces
+		if(!a) return "";
+		return a.replace(/ +/g, " ")
+				.replace(/[\t]+/gm, "")
+				.replace(/[ ]+$/gm, "")
+				.replace(/^[ ]+/gm, "")
+				.replace(/\n+/g, "\n")
+				.replace(/\n+$/, "")
+				.replace(/^\n+/, "")
+				.replace(/\nNEWLINE\n/g, "\n\n")
+				.replace(/NEWLINE\n/g, "\n\n"); // IE
+	}
+	var removeWhiteSpace = function(node){
+		// getting rid of empty text nodes
+		var isWhite = function(node) {
+			return !(/[^\t\n\r ]/.test(node.nodeValue));
+		}
+		var ws = [];
+		var findWhite = function(node){
+			for(var i=0; i<node.childNodes.length;i++){
+				var n = node.childNodes[i];
+				if (n.nodeType==3 && isWhite(n)){
+					ws.push(n)
+				}else if(n.hasChildNodes()){
+					findWhite(n);
+				}
+			}
+		}
+		findWhite(node);
+		for(var i=0;i<ws.length;i++){
+			ws[i].parentNode.removeChild(ws[i])
+		}
+
+	}
+	var sty = function(n, prop){
+		// Get the style of the node.
+		// Assumptions are made here based on tagName.
+		if(n.style[prop]) return n.style[prop];
+		var s = n.currentStyle || n.ownerDocument.defaultView.getComputedStyle(n, null);
+		if(n.tagName == "SCRIPT") return "none";
+		if(!s[prop]) return "LI,P,TR".indexOf(n.tagName) > -1 ? "block" : n.style[prop];
+		if(s[prop] =="block" && n.tagName=="TD") return "feaux-inline";
+		return s[prop];
+	}
+
+	var blockTypeNodes = "table-row,block,list-item";
+	var isBlock = function(n){
+		// diaply:block or something else
+		var s = sty(n, "display") || "feaux-inline";
+		if(blockTypeNodes.indexOf(s) > -1) return true;
+		return false;
+	}
+	var recurse = function(n){
+		// Loop through all the child nodes
+		// and collect the text, noting whether
+		// spaces or line breaks are needed.
+		if(/pre/.test(sty(n, "whiteSpace"))) {
+			t += n.innerHTML
+				.replace(/\t/g, " ")
+				.replace(/\n/g, " "); // to match IE
+			return "";
+		}
+		var s = sty(n, "display");
+		if(s == "none") return "";
+		var gap = isBlock(n) ? "\n" : " ";
+		t += gap;
+		for(var i=0; i<n.childNodes.length;i++){
+			var c = n.childNodes[i];
+			if(c.nodeType == 3) t += c.nodeValue;
+			if(c.childNodes.length) recurse(c);
+		}
+		t += gap;
+		return t;
+	}
+	// Use a copy because stuff gets changed
+	node = node.cloneNode(true);
+	// Line breaks aren't picked up by textContent
+	node.innerHTML = node.innerHTML.replace(/<br>/g, "\n");
+
+	// Double line breaks after P tags are desired, but would get
+	// stripped by the final RegExp. Using placeholder text.
+	var paras = node.getElementsByTagName("p");
+	for(var i=0; i<paras.length;i++){
+		paras[i].innerHTML += "NEWLINE";
+	}
+
+	var t = "";
+	removeWhiteSpace(node);
+	return normalize(recurse(node));
+}
+
 function removeBR(input) {							/* Used with Gecko */
 	var output = "";
 	for (var i = 0; i < input.length; i++) {
@@ -120,19 +236,17 @@ function removeBR(input) {							/* Used with Gecko */
 }
 
 function downloadable_snippet(mySelection, fname) {
+    matches = fname.match(/\.(\w+)$/);
+    var ext = matches ? matches[1] : 'php';
 
+    var media_file = 'mediafile mf_' + ext;
 
-matches = fname.match(/\.(\w+)$/);
-var ext = matches ? matches[1] : 'php';
+    var selection = '<dl class="file">' +
+        '<dt><a href="doku.php?do=export_code&id=start&codeblock=0"' 
+              + 'title="Download Snippet" class="' + media_file + '">' + fname +'</a></dt>'
+        + '<dd>'
+        + mySelection
+       + ' </dd> </dl>';
 
-var media_file = 'mediafile mf_' + ext;
-
-var selection = '<dl class="file">' +
-    '<dt><a href="doku.php?do=export_code&id=start&codeblock=0"' 
-          + 'title="Download Snippet" class="' + media_file + '">' + fname +'</a></dt>'
-    + '<dd>'
-    + mySelection
-   + ' </dd> </dl>';
-
- return selection;
+    return selection;
 }
