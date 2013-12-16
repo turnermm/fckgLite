9c1a09cd89004b2475b4a633e0ceb829b48f60e0
Myron Turner <turnermm02@shaw.ca>
2013-12-15 20:24:51 -0600

    Remove ~~COMPLEX_TABLES~~ macro in save.php and disable macro checkbox if complex set in config manager

--- action/edit.php
+++ action/edit.php
@@ -739,7 +739,9 @@ global $INFO;
     <?php  global $useComplexTables;  if($useComplexTables) { ?>               
         document.getElementById('complex_tables').click();            
     <?php } ?>  
-
+    <?php  if($this->getConf('complex_tables')) { ?>
+         document.getElementById('complex_tables').disabled = true;
+    <?php } ?> 
    
 var fckgLPluginPatterns = new Array();
 
