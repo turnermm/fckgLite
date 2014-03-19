cc24da793bcb6cd0bfc597305cc72f5b429ed94c
Myron Turner <turnermm02@shaw.ca>
2014-03-19 11:45:50 -0500

    add new language files in fckg/lang

---
+++ lang/ar/lang.php
@@ -0,0 +1,31 @@
+<?php
+
+$lang['btn_dw_edit'] = "المحرر النصي";
+$lang['dw_btn_fck_preview']="معاينة FCK";
+$lang['dw_btn_lang']="اللغة";
+$lang['title_dw_delete'] = "احذف الصفحة";
+$lang['title_dw_edit'] = "احفظ العمل، أخرج، وحول إلى المحرر النصي الافتراضي";
+// new
+$lang['dw_btn_revert'] = "تراجع";
+$lang['title_dw_revert'] = "تراجع للنسخ الاحتياطي السابق";
+$lang['title_dw_lang']="اختر لغة للمددق الإملائي";
+$lang['title_dw_cancel']='اخرج من المحرر';
+$lang['btn_fck_edit'] = "المحرر الرسومي";
+$lang['confirm_delete'] = "أمتأكد من رغبتك بحذف هذه الصفحة؟";
+$lang['confirm_preview'] = "ستخسر كل مالم تحفظه.";
+//new2
+$lang['dw_btn_backup'] ="انسخ احتياطيا";
+$lang['title_dw_backup'] ="انسخ محتوى المحرر احتياطيا ونشط قفل الصفحة";
+//new 3
+$lang['backup_empty'] = "محفوظات النسخ الاحتياطي فارغة. هل تريد الإستمرار?";
+$lang['btn_draft'] = "اجلب المسودة";
+//new 4
+$lang['title_draft'] = "اعرض, أظهر, حرر المسودة";
+$lang['btn_exit_draft'] = "اخرج من المسودة";
+$lang['title_draft'] = "اعرض, أظهر, حرر المسودة";
+
+//new 5
+$lang['title_exit_draft'] = "ارجع للمستند الحالي";
+$lang['draft_msg']= "هناك مسودة لهذا المستند. زر المسودة يبدل بين هذا المستند والمسودة. يمكنك حفظ وتحرير أيا منهما." ;
+
+
