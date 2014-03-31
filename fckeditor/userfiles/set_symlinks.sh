7ecc168537f1ed11cee3bb42bb057ae1ea6198c6
Myron Turner <turnermm02@shaw.ca>
2014-03-31 16:22:55 -0500

    removed hard-coded symlinks from userfiles

---
+++ fckeditor/userfiles/set_symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# run this from the command line in the current directory (userfiles): ./set_symlinks.sh
+# this script must be set to executable and it assumes that your data edirectory has not been moved
+ln -s ../../../../../data/media file
+ln -s ../../../../../data/media image
+ln -s ../../../../../data/media media
+ln -s ../../../../../data/media flash
+
