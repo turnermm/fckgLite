3605e15094ef7e2b189ba806d2ee469a156cebb7
Matthieu KIM <mkim@kimkyouk.fr>
2014-09-05 17:20:05 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -5,14 +5,18 @@
  * 
  * @author Matthieu Jouan <mat.jouan@gmail.com>
  * @author Caillot <remicaillot5@gmail.com>
+ * @author Matthieu KIM <mkim@kimkyouk.fr>
  */
 $lang['groups']                = 'Groupe avec la permission de désactiver le verrouillage temporisé (obsolète)';
 $lang['middot']                = 'Liste de groupes séparés par des virgules employant le &amp;middot; pour le &amp;nbsp;';
 $lang['guest_toolbar']         = 'Afficher la barre d\'outils aux invités';
-$lang['guest_media']           = 'Le invités peuvent liés vers des fichiers de média';
+$lang['guest_media']           = 'Les invités peuvent créer des liens vers des fichiers multimédias';
 $lang['open_upload']           = 'Les invités peuvent déposer des fichiers';
 $lang['smiley_as_text']        = 'Afficher les smileys en tant que texte dans l\'éditeur FCKeditor (elle seront toujours affichée en temps qu\'images dans le navigateur)';
-$lang['editor_bak']            = 'écrire la sauvegarde dans meta/&lt;namespace&gt;.fckg';
-$lang['create_folder']         = 'Activer le bouton de création de répertoire dans l\'explorateur de document (o/n)';
+$lang['editor_bak']            = 'Enregistrer la sauvegarde dans meta/&lt;namespace&gt;.fckg';
+$lang['create_folder']         = 'Activer le bouton de création de répertoire dans l\'explorateur de fichiers (o/n)';
+$lang['auth_ci']               = 'Le nom d\'utilisateur n\'est pas sensible à la casse ce qui veut dire que vous pouvez vous connecter en tant que UTILISATEUR ou utilisateur';
 $lang['no_symlinks']           = 'Désactiver la création automatique de liens symboliques dans fckg/userfiles.';
+$lang['scayt_auto']            = 'Active automatiquement le correcteur orthographique SCAYT. "on" par défaut. Pour le désactiver, sélectionner "off"';
+$lang['scayt_lang']            = 'Définir la langue par défaut de SCAYT.';
 $lang['dw_priority']           = 'Faire de Dokuwiki l\'éditeur par défault';
