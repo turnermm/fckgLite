46a1ea25ed3379df2dff0dce07fef065c4d3d09f
Jonathon Armbruster <jonathon.armbruster@itstrianing.co.uk>
2014-10-29 14:05:05 +0100

    translation update

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -1,5 +1,8 @@
 <?php
 
-$lang['groups'] = "lista de grupos con el permiso de inhabilitar el cerradura-contador de tiempo";
-$lang['middot'] ="lista de los grupos que usan el &amp;middot; para el &amp;nbsp;";
-
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['groups']                = 'lista de grupos con el permiso de inhabilitar el cerradura-contador de tiempo';
+$lang['middot']                = 'lista de los grupos que usan el &amp;middot; para el &amp;nbsp;';
