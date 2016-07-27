b393527ea12e64a98dc9ffc3b61d0f9f03120747
Myeongjin <aranet100@gmail.com>
2016-07-27 12:40:19 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -7,14 +7,14 @@
  */
 $lang['groups']                = '잠금 타이머를 비활성화하도록 허용하는 그룹 (사용되지 않음)';
 $lang['fck_preview']           = 'FCK 미리 보기 그룹';
-$lang['guest_toolbar']         = '손님에게 도구 모음을 보여주기';
+$lang['guest_toolbar']         = '손님에게 도구 모음을 표시';
 $lang['guest_media']           = '손님이 미디어 파일로 링크할 수 있음';
 $lang['open_upload']           = '손님이 올릴 수 있음';
 $lang['xcl_plugins']           = '변할 수 없는 문법 플러그인의 쉼표로 구분된 목록. 플러그인의 이름은 fckg 관리 패널의 플러그인 목록에서 찾을 수 있는 현재 설치된 플러그인의 목록에 나타나는 것과 정확해야 합니다.';
 $lang['default_fb']            = '기본 파일 찾아보기 접근. none이면, acl이 적용되지 않습니다.';
 $lang['openfb']                = '파일 찾아보기를 엽니다. 사용자가 권한이 있는지 없는지에 따라 사용자에게 전체 디렉터리 구조에 접근할 수 있도록 줍니다. ACL은 여전히 올리기에 적용됩니다.';
 $lang['dw_edit_display']       = '"DW 편집" 버튼으로 접근할 수 있는 사용자를 제어합니다. 선택: "all"은 모든 사용자; "admin"은 admin 및 manager만; "none"은 아무도 없음. 기본값은 "all"입니다.';
-$lang['smiley_as_text']        = 'FCKeditor에서 이모티콘을 텍스트로 보여주기 (여전히 브라우저에서는 그림으로 보여집니다)';
+$lang['smiley_as_text']        = 'FCKeditor에서 이모티콘을 텍스트로 표시 (여전히 브라우저에서는 그림으로 표시됩니다)';
 $lang['editor_bak']            = 'meta/&lt;namespace&gt;.fckg에 백업을 저장';
 $lang['create_folder']         = '파일 브라우저에서 폴더 만들기 버튼 활성화 (y/n)';
 $lang['dwedit_ns']             = 'FckgLite가 네이티브 도쿠위키 편집기로 자동으로 전환할 이름공간의 쉼표로 구분된 목록.';
