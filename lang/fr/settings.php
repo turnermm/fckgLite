e8349dee52397b96e7a8eedcc0ee82eccb718428
Nutakor <lvnutakor@gmail.com>
2018-02-21 15:35:38 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -2,22 +2,30 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Nutakor <lvnutakor@gmail.com>
  * @author Matthieu Jouan <mat.jouan@gmail.com>
  * @author Caillot <remicaillot5@gmail.com>
  * @author Matthieu KIM <mkim@kimkyouk.fr>
  * @author Syl <syl@mailoo.org>
  */
 $lang['groups']                = 'Groupe avec la permission de désactiver le verrouillage temporisé (obsolète)';
-$lang['middot']                = 'Liste de groupes séparés par des virgules employant le &amp;middot; pour le &amp;nbsp;';
 $lang['guest_toolbar']         = 'Afficher la barre d\'outils aux invités';
 $lang['guest_media']           = 'Les invités peuvent créer des liens vers des fichiers multimédias';
 $lang['open_upload']           = 'Les invités peuvent déposer des fichiers';
+$lang['xcl_plugins']           = 'Liste de valeurs séparées par des virgules de plugins Immutable Syntax. Leurs noms doivent correspondre précisément à ceux dans la Liste des Greffons Installés trouvés sur le panneau d\'administration de fckg, dans la liste de tous les greffons.';
+$lang['dw_edit_display']       = 'Détermine les utilisateurs ayant accès au bouton "Édition DW". Options : "tous" pour tous les utilisateurs; "admin" pour seuls l\'administrateur et les gérants; "aucun" pour aucun.';
 $lang['smiley_as_text']        = 'Afficher les smileys en tant que texte dans l\'éditeur FCKeditor (elle seront toujours affichée en temps qu\'images dans le navigateur)';
 $lang['editor_bak']            = 'Enregistrer la sauvegarde dans meta/&lt;namespace&gt;.fckg';
 $lang['create_folder']         = 'Activer le bouton de création de répertoire dans l\'explorateur de fichiers (o/n)';
+$lang['dwedit_ns']             = 'Liste de valeurs séparées par des virgules d\'espaces-nom pour lesquels FckgLite commute automatiquement vers l\'Editeur DokuWiki natif.';
+$lang['acl_del']               = 'Par défaut (case non cochée) permet aux utilisateurs avec autorisation de téléchargement de supprimer des fichiers multimédias; si la case est cochée, l\'utilisateur nécessite l\'autorisation de suppression pour effacer du contenu dans le dossier.';
 $lang['auth_ci']               = 'Le nom d\'utilisateur n\'est pas sensible à la casse ce qui veut dire que vous pouvez vous connecter en tant que UTILISATEUR ou utilisateur';
+$lang['nix_style']             = 'Pour Windows Servers (Vista et ultérieur). Ce paramètre permet d\'accéder à data\media au travers de fckgckeditor\userfiles, si des liens vers les fichiers et fichiers multimédias ont bien étés créés dans userfiles.';
 $lang['no_symlinks']           = 'Désactiver la création automatique de liens symboliques dans fckg/userfiles.';
+$lang['direction']             = 'Établir le sens de lecture dans le FCKeditor: <b>nocheck</b>: fckgLite ne modifiera pas les paramètres de sens par défaut; <b>dokuwiki</b>: le sens de lecture actuel de Dokuwiki; <b>ltr</b>: De gauche à droite; <b>rtl</b>: De droite à gauche.';
 $lang['scayt_auto']            = 'Active automatiquement le correcteur orthographique SCAYT. "on" par défaut. Pour le désactiver, sélectionner "off"';
+$lang['scayt']                 = 'Utiliser le vérificateur orthographique SCAYT. Ceci est "on" par défaut; Selectionner "off" basculera vers le vérificateur orthographique spellerpages.';
 $lang['scayt_lang']            = 'Définir la langue par défaut de SCAYT.';
 $lang['dw_priority']           = 'Faire de Dokuwiki l\'éditeur par défaut';
+$lang['winstyle']              = 'Utiliser le chemin direct vers le répertoire multimédia au lieu de fckeditor/userfiles. Ceci nécessite que fckeditor/userfiles/.htaccess.security soit copié vers data/media et renommé .htaccess';
